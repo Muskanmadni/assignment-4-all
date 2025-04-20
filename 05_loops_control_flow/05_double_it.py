fcc25dad4ce2ece9c9a20ec5b4f7f0fd4affb20d
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2025-04-20 22:33:15 +0500

    Add files via upload

---
+++ 05_loops_control_flow/05_double_it.py
@@ -0,0 +1,10 @@
+
+def main():
+    number = int(input("Enter a number: "))
+    while number < 100:
+        print(number * 2)
+        number = int(input("Enter a number: "))
+
+
+if __name__ == "__main__":
+    main()
